ced79334178fa226b35e885536224ac6cd6c82da
anas-ai <anasrizvi4206@gmail.com>
2025-08-11 13:28:06 +0530

    fix

--- src/config/apiConfig.tsx
+++ src/config/apiConfig.tsx
@@ -1,11 +1,13 @@
-// export const BASE_URL = 'https://staging3.mortgagetools.ca/api';
 
 import Config from 'react-native-config';
 
 export const ApiConfig = {
   LOGIN_API: `${Config.BASE_URL}/login`,
+
   SIDEBAR_MENU_API: (role: number) =>
     `${Config.BASE_URL}/get/sidebar/menu?role=${role}`,
+
   LOGOUT_API: `${Config.BASE_URL}/logout`,
-  DASHBOARD_API:(userId:number,roleId:number)=> `${Config.BASE_URL}/dashboard/${userId}/${roleId}`,
+  DASHBOARD_API: (userId: number, roleId: number) =>
+    `${Config.BASE_URL}/dashboard/${userId}/${roleId}`,
 };
